8bf74ee1e5650f08efe8d7f4be080e875ffbee93
B. Martin <imbrittanyalexis@gmail.com>
2024-06-10 21:36:43 -0400

    updated index.js

--- models/index.js
+++ models/index.js
@@ -1,19 +1,24 @@
+const Sequelize = require('sequelize');
+
 const User = require('./user');
 const Questions = require('./questions');
 const Answers = require('./answers');
+const Scores = require('./scores');
 
-User.hasMany(Questions, {
+User.hasMany(Answers);
+Answers.belongsTo(User);
 
-});
+Questions.hasMany(Answers);
+Answers.belongsTo(Questions);
 
-Questions.belongsTo(User, {
+Questions.hasOne(Answers, { as: "correctAnswer" });
 
-}); 
 
-User.hasMany(Answers, {
-
-}); 
-
-Answers.belongsTo(User, {
-
-}); 
+sequelize
+  .sync({ force: false }) // Set force to true to drop existing tables and recreate them
+  .then(() => {
+    console.log("Database synchronized successfully");
+  })
+  .catch((error) => {
+    console.error("Error synchronizing database:", error);
+  });
