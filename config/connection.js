5719765f3b2de17c76934471acd1b72521439e59
rozierhj <160989130+rozierhj@users.noreply.github.com>
2024-06-17 19:51:06 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -1,52 +1,52 @@
 
-// const { Sequelize } = require('sequelize');
-// require('dotenv').config();
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//     host: process.env.DB_HOST || 'localhost',
-//     dialect: 'postgres',
-//     port: process.env.DB_PORT || 5432,
-// });
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432,
+});
 
-// const testConnection = async () => {
-//     try {
-//         await sequelize.authenticate();
-//         console.log('Connection has been established successfully.');
-//     } catch (error) {
-//         console.error('Unable to connect to the database:', error);
-//         process.exit(1); // Exit the process with an error code
-//     }
-// };
+const testConnection = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+        process.exit(1); // Exit the process with an error code
+    }
+};
 
-// testConnection();
-
-// module.exports = sequelize;
-
-
-
-
-
-
-
-
-
-
-
-
-
-const Sequelize = require("sequelize");
-require("dotenv").config();
-
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: "localhost",
-        dialect: "postgres",
-      }
-    );
+testConnection();
 
 module.exports = sequelize;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Sequelize = require("sequelize");
+// require("dotenv").config();
+
+// const sequelize = process.env.DB_URL
+//   ? new Sequelize(process.env.DB_URL)
+//   : new Sequelize(
+//       process.env.DB_NAME,
+//       process.env.DB_USER,
+//       process.env.DB_PASSWORD,
+//       {
+//         host: "localhost",
+//         dialect: "postgres",
+//       }
+//     );
+
+// module.exports = sequelize;
